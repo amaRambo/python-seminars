a65039ef30e85b2e28864dfe662a6ad33f09e9aa
Ilya Tsai <ilya_1_3_5@mail.ru>
2022-12-12 23:36:10 +0500

    3 4 5 added

--- sem1/task2.py
+++ sem1/task2.py
@@ -16,3 +16,33 @@ while i < 4:
         max = s
     i = i + 1
 print(max)
+
+
+# через массив
+# import array
+
+# arr = []
+# for i in range(5):
+#     arr.append(int(input(f"Введите число {i+1}: ")))
+# max = arr[0];
+# for i in range(1,5):
+#     if arr[i]>max:1
+#         max = arr[i]    
+# print(max)
+#Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+# a = int(input())
+# b = int(input())
+# if a**2==b or b**2==a:
+#     print("yes")
+# else:
+#     print("no")
+# #Напишите программу, которая на вход принимает 5 чисел и находит максимальное из них.
+# # print(max(int(input()),int(input()),int(input())))
+# a = int(input())
+# maxx = a
+# for i in range(4):
+#     a = int(input())
+#     if a>maxx:
+#         maxx = a
+# print(maxx)
