3796d72cde0b6e822b51add03fb4085e77b53afc
ilya <ilya_1_3_5@mail.ru>
2023-01-30 00:57:17 +0500

    not work

--- sem9/tg1.py
+++ sem9/tg1.py
@@ -1,9 +1,9 @@
 import telebot
 from telebot import types
 import random
-
+from random import randint
 bot = telebot.TeleBot("5804587400:AAELkmE4RmkQKUl6745HozMTO-DfSZfH0d4")
-a = 0
+
 @bot.message_handler(commands = ["start"])
 def start(message):
     bot.send_message(message.chat.id,"/button")
@@ -21,17 +21,24 @@ def button(message):
 def controller(message):
     print(message.text)
     if message.text == "узнать правила игры":
-        bot.send_message(message.chat.id,"Создайте программу для игры с конфетами человек против человека.Условие задачи: На столе лежит 2021 конфета. Играют два игрока делая ход друг после друга. Первый ход определяется жеребьёвкой. За один ход можно забрать не более чем 28 конфет. Все конфеты оппонента достаются сделавшему последний ход. Сколько конфет нужно взять первому игроку, чтобы забрать все конфеты у своего конкурента?")
+        bot.send_message(
+            message.chat.id,
+            "На столе лежит 2021 конфета. Играют два игрока делая ход друг после друга."
+            "Первый ход определяется жеребьёвкой. За один ход можно забрать не более чем 28 конфет." 
+            "Все конфеты оппонента достаются сделавшему последний ход."
+        )
     elif message.text == "играть":
-        bot.send_message(message.chat.id,"давай начнем")
+        # bot.send_message(message.chat.id,"давай начнем")
         bot.register_next_step_handler(message,play)
 
 def play(message):
-    global a
+   
     name1 = 'Вы'
     name2 = "bot"
     turn = 0
-    sweets = 2021 
+    global a
+    a = 0
+    sweets = 200
     max_sweet = 28
     first_turn = random.choice([name1, name2])
     flag = name1 if first_turn == name1 else name2
@@ -42,39 +49,42 @@ def play(message):
             print(f"Ваш ход {flag}")
 
             if flag == name1:
-                # turn = int(input("введите желаемое количество конфет для взятия- "))
                 bot.send_message(message.chat.id,"Введите кол-во конфет")
-                bot.register_next_step_handler(message,user_turn)
-                turn = a
-                while not 0<turn<max_sweet:
-                    bot.send_message(message.chat.id,f"Введите конфеты в диапазоне от 0 до {max_sweet}")
-                    bot.send_message(message.chat.id,"Введите кол-во конфет")
-                    bot.register_next_step_handler(message,user_turn)
-                    turn = a
+                # bot.register_next_step_handler(message,user_turn)
+                turn = int(message.text)
+                # while not 0<turn<max_sweet:
+                #     bot.send_message(message.chat.id,f"Введите конфеты в диапазоне от 0 до {max_sweet}")
+                #     bot.register_next_step_handler(message,user_turn)
+                    
                 sweets -= turn
                 if sweets>0:
                     bot.send_message(message.chat.id,f'конфет осталось - {sweets}')
                 else:
                     bot.send_message(message.chat.id,f'конфет осталось - 0')
-
+                bot.send_message(message.chat.id,f"вы взяли {turn} конфет")
+                sweets -= turn
                 flag = name2 if flag == name1 else name1
             else:
                 turn = random.randint(1,max_sweet)
                 bot.send_message(message.chat.id,f"bot взял {turn} конфет")
                 sweets -= turn
                 if sweets > 0:
-                    print(f'конфет осталось - {sweets}')
+                    bot.send_message(message.chat.id,f'конфет осталось - {sweets}')
                 else:
-                    print(f'конфет осталось - 0')
+                    bot.send_message(message.chat.id,'конфет осталось - 0')
 
                 flag = name2 if flag == name1 else name1
 
     winner = name2 if flag == name1 else name1
-    print(f"Поздравляем победил игрок {winner}")
+    bot.send_message(message.chat.id,f"Поздравляем победил игрок {winner}")
 
 def user_turn(message):
     global a
     a = int(message.text)
+    
+    
+
+   
 
 
 bot.infinity_polling()
